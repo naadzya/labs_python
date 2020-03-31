9afce111b502d79f60dbb6439fc06b89131cf5a2
Nadzya Hryshalevich <nadyagrishalevich@gmail.com>
2020-03-31 16:24:46 +0300

    Final

--- lab_2/task_8_poweroftwo.py
+++ lab_2/task_8_poweroftwo.py
@@ -1,32 +1,34 @@
-#Task 8
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
 import time
+import sys
 
-def calculate_time(func):
-    """Calculates the time during use the function func
-    """
-    def inner(*args, **kwargs):
-        start = time.time()
-        value = func(*args, **kwargs)
-        ending = time.time()
-        print(f'It takes {ending-start} seconds')
-        return value
-    return inner
+__all__ = ['is_power_of_two']
 
-@calculate_time
 def is_power_of_two(n: int):
     inbin = str(bin(n))[3:]
     return inbin == '0'*len(inbin)
 
 def main():
-    val = input('Enter your number: ')
-    while val != 'exit':
-        while not val.isdigit():
-            print('Wrong input. Try again')
-            val = input()
-        print('It\'s power of 2' if is_power_of_two(int(val))
-              else 'It\'s not power of two')
-        val = input('Enter your number or write exit to \
+    args = sys.argv[1:]
+    if not args:
+        val = input('Enter your number: ')
+        while val != 'exit':
+            while not val.isdigit():
+                print('Wrong input. Try again')
+                val = input()
+            print('It\'s power of 2' if is_power_of_two(int(val))
+                  else 'It\'s not power of two')
+            val = input('Enter your number or write exit to \
 close the programm: ')
+        return
+    val = args[0]
+    if not val.isdigit():
+        print('Wrong input')
+        return
+    print('It\'s power of 2' if is_power_of_two(int(val))
+                  else 'It\'s not power of two')
 
 if __name__ == '__main__':
     main()
