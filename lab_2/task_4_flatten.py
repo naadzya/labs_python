6275d18343917a76bab86f7408394bca6ab60338
Nadzya Hryshalevich <62619513+naadzya@users.noreply.github.com>
2020-04-12 17:13:41 +0300

    Generator + using argparse

--- lab_2/task_4_flatten.py
+++ lab_2/task_4_flatten.py
@@ -1,52 +1,79 @@
 #Task 4
-import time
+import argparse
 
-def calculate_time(func):
-    """Calculates the time during use the function func
-    """
-    def inner(*args, **kwargs):
-        start = time.time()
-        value = func(*args, **kwargs)
-        ending = time.time()
-        print('\nDone! It takes %.5f seconds' % (ending-start))
-        return value
-    return inner
+__all__ = ['flatten_it', 'flatten']
 
-def is_there_iter(A):
-    """Returns True if there is at least one nested list in A
-    """
-    return any(isinstance(i, (list, tuple)) for i in A)
-
-@calculate_time
 def flatten_it(A):
-    """Returns a copy of an given list, tuple or their combinations
+    """Generator for flatten"""
+    if isinstance(A, (list, tuple)):
+        F = [iter(A)]        # Create an iterator
+        while F:
+            for it in F[-1]:    # The deepest list
+                if isinstance(it, (list, tuple)):
+                    F.append(iter(it))
+                    break
+                else:
+                    yield it
+            else:
+                F.pop()
+
+def flatten(A):
+    """
+    Returns a copy of an given list, tuple or their combinations
     collapsed into one dimension
     """
-    if isinstance(A, (list, tuple)):
-        F = list(A)        #Copy of A
-        while is_there_iter(F):   #Flattens while there won't be
-            i = 0                 #any nested objects
-            while i < len(F):
-                if isinstance(F[i], (list, tuple)):
-                    F[i] = list(F[i])
-                    nested = F.pop(i)       #Removes the nested list
-                    for j in range(len(nested)):   #Inserts all elements
-                        F.insert(i+j, nested[j])   #from the nested list
-                i += 1
-        if isinstance(A, tuple):
-            return tuple(F)
-        return F
+    flatt = []
+    for x in flatten_it(A):
+        flatt.append(x)
+    return tuple(flatt) if isinstance(A, tuple) else flatt
+    
                     
 def test_flatten_it():
     """A test for the function flatten_it
     """
     arr = ([1, 2, (3, (4, 5, [6]), 7), [8, [[9, 10]]], 11,
            [12, (13, [14, [[15]]]), 16], [17, [[(18), 19]]]], 20)
-    flattarr = flatten_it(arr)
-    print(f'The original:\n{arr}', f'Flattened:\n{flattarr}', sep='\n')    
+    flatt = flatten(arr)
+    print(f'The original:\n{arr}', f'Flattened:\n{flatt}', sep='\n')    
 
 def main():
-    test_flatten_it()
+    parser = argparse.ArgumentParser(description=
+                                     'Your list or tuple that needs to be flattened')
+    parser.add_argument('-arr', '--array', type=str, help='Needs to be flattened')
+    args = parser.parse_args()
+    val = args.array
+    if not val:
+        val = input('Enter a list or tuple:\n')
+        while True:
+            try:
+                eval(val)
+            except (NameError, SyntaxError):
+                print('Wrong input, try again')
+                val = input('Enter your list or tuple:\n')
+            else:
+                val = eval(val)
+                if not isinstance(val, (list, tuple)):
+                    print('Wrong input, try again')
+                    val = input('Enter a list or tuple:\n')
+                else: break
+        print(f'Flattened:\n{flatten(val)}')
+        return
+    while True:
+        try:
+            val = eval(val)
+        except (NameError, SyntaxError):
+            print('Wrong input, try again')
+            val = input('Enter your list or tuple:\n')
+        else:
+            if not isinstance(val, (list, tuple)):
+                print('Wrong input, try again')
+                val = input('Enter a list or tuple:\n')
+            else:
+                break   
+    print(f'Flattened:\n{flatten(val)}')
 
 if __name__ == "__main__":
     main()
+
+
+
